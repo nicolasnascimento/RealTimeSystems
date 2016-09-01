3ae96c135d9d99feff31f801a4250fe508faa267
NICOLAS PEREIRA DO NASCIMENTO <13111476@l3332087.portoalegre.pucrsnet.br>
2016-09-01 18:56:04 -0300

    Pressao

---
+++ T1/Task.h
@@ -0,0 +1,13 @@
+#ifndef TASK_H
+#define TASK_H
+
+typedef struct PeriodTask {
+	int computingTime;
+	int periodTime;
+	int deadlineTime;
+} PeriodTask;
+
+typedef struct APeriodTask {
+} APeriodTask ;
+
+#endif // TASK_H
